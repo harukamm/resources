070e751f2677dc314513cd1b7084e5dce853f312
Matsumoto Haruka <harukam0416@gmail.com>
2019-08-01 12:07:03 +0900

    Add white noise related scripts

---
+++ scripts/play.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/ruby
+
+def kill_olds(pattern)
+  IO.popen("ps ax | grep -E \"#{pattern}\" | grep -v grep | awk '{ print $1 }'") do |pipe|
+    pipe.each do |pid|
+      puts pid
+      begin
+        Process.kill(9, pid.to_i)
+        puts "[Log] Killed process #{pid}"
+      rescue => e
+        puts "[Error] Failed to kill pid #{pid} with error #{e}"
+        raise e
+      end
+    end
+  end
+end
+
+def play(fname, volume)
+  path = "~/white-noise"
+  fpath = "#{path}/#{fname}"
+  kill_olds("afplay.+#{fname}")
+  if volume == 0
+    return
+  end
+  pid = spawn("while true; do sleep 0.1; afplay #{fpath} -t 100000 -v #{volume}; done")
+  Process.detach(pid)
+end
+
+vol = (ARGV[0] ? ARGV[0] : 1).to_i
+fname = "white-noise.mp3"
+
+case ARGV[1]
+when "talk"
+  fname = "nz_talk.mp3"
+when "cafe"
+  fname = "nz_cafe.mp3"
+end
+
+play(fname, vol)
